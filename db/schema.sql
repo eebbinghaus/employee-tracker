6463046ceeb1e561dfa43c7ab29d2fae2dc0fc53
Erich Ebbinghaus <eebbinghaus@gmail.com>
2022-12-03 10:51:24 -0700

    added prompt questions for add a dept., add a role, add emp. and update an emp. role

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,36 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES  department(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const express = require("express");
 const cTable = require("console.table");
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const navQuestion = [
   {
@@ -26,6 +19,98 @@ const navQuestion = [
   },
 ];
 
+const addPrompt = [
+  {
+    type: "input",
+    message:
+      "Please enter the name of the Department that you would like to add:",
+    name: "department",
+  },
+];
+
+const addRole = [
+  {
+    type: "input",
+    message: "What is the name of the new Role that you would like to add?",
+    name: "role",
+  },
+  {
+    type: "input",
+    message: "What is the salary of this new role?",
+    name: "salary",
+  },
+  {
+    type: "list",
+    message: "Which Department would you like to add this role to?",
+    choices: ["Offense", "Defense", "Special Teams"],
+    name: "role",
+  },
+];
+
+const addEmployee = [
+  {
+    type: "input",
+    message: "What is the employee's first name?",
+    name: "first",
+  },
+  {
+    type: "input",
+    message: "What is the employee's last name?",
+    name: "last",
+  },
+  {
+    type: "list",
+    message: "What is the employees roll?",
+    choices: ["Offense", "Defense", "Special Teams"],
+    name: "role",
+  },
+  {
+    type: "list",
+    message: "Who is the Employees Manager?",
+    choices: ["Bill Walsh", "Bill Belichick", "Scott O'Brien"],
+    name: "manager",
+  },
+];
+
+const updateEmployee = [
+  {
+    type: "list",
+    message: "Which Employee's role would you like to update?",
+    choces: [
+      "John Elway",
+      "Barry Sanders",
+      "Jerry Rice",
+      "Von Miller",
+      "Ray Lewis",
+      "Ed Reed",
+      "Jason Elam",
+      "Ray Guy",
+      "Bill Walsh",
+      "Bill Belichick",
+      "Scott O'Brien",
+    ],
+    name: "employee",
+  },
+  {
+    type: "list",
+    message: "What Role would you like to assign to the selected Employee?",
+    choices: [
+      "Quarter Back",
+      "Running Back",
+      "Wide Receiver",
+      "Defensive End",
+      "Linebacker",
+      "Safety",
+      "Kicker",
+      "Punter",
+      "Offensive Coordinator",
+      "Defensive Coordinator",
+      "Special Teams Coordinator",
+    ],
+    name: "role",
+  },
+];
+
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -39,40 +124,63 @@ const db = mysql.createConnection(
 function navigation() {
   inquirer.prompt(navQuestion).then((response) => {
     if (response.nav === "View All Departments") {
-      db.query("SELECT * FROM department", function (err, results) {
-        // console.log(results);
-        console.table(results);
-      });
-      navigation();
+      db.query(
+        `SELECT department.name AS "Department", department.id AS "Department id" FROM department`,
+        function (err, results) {
+          if (err) {
+            console.log(err);
+            throw new Error("Something went wrong!");
+          }
+          // console.log(results);
+          console.table(results);
+          navigation();
+        }
+      );
     }
     if (response.nav === "View All Roles") {
-      db.query("SELECT * FROM role", function (err, results) {
-        // console.log(results);
-        console.table(results);
-      });
-      navigation();
+      db.query(
+        `SELECT role.title AS "Job Title", role.id AS "Role id", department.name AS "Department", role.salary AS "Salary" FROM department, role WHERE department.id=role.department_id`,
+        function (err, results) {
+          //   console.log(results);
+          console.table(results);
+          navigation();
+        }
+      );
     }
     if (response.nav === "View All Employees") {
-      db.query("SELECT * FROM employee", function (err, results) {
-        // console.log(results);
-        console.table(results);
+      db.query(
+        ` SELECT employee.id AS "Employee id", employee.first_name AS "First Name", employee.last_name AS "Last Name", role.title AS "Job Title", department.name AS "Department",
+      role.salary AS "Salary", employee.manager_id AS "Manager"
+      FROM department
+      JOIN role
+      ON department.id=role.department_id
+      JOIN employee
+      ON role.id=employee.role_id`,
+        function (err, results) {
+          // console.log(results);
+          console.table(results);
+          navigation();
+        }
+      );
+    }
+    if (response.nav === "Add a Department") {
+      inquirer.prompt(addPrompt).then((response) => {
+        db.query(
+          `INSERT INTO department (name) VALUES ("${response.department}");`,
+          function (err, results) {
+            if (err) {
+              console.log(err);
+              throw new Error("Something went wrong!");
+            }
+            console.log(
+              `${response.department} has been added to Departments!`
+            );
+            navigation();
+          }
+        );
       });
-      navigation();
     }
   });
 }
 
 navigation();
-
-// db.query("SELECT * FROM employee", function (err, results) {
-//   console.log(results);
-//   console.table(results);
-// });
-
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
